99762f3ad42f601ea842758fe47c9f3c78078ff3
ZydrunasK <kniukstaz@gmail.com>
2024-10-15 08:05:19 +0300

    up

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,20 +1,32 @@
 import s from './login.module.css';
+import logo from '/src/assets/vite.svg';
 
 export function Login() {
     return (
         <>
             <div className={s.cont}>
-                <form action="">
-                    <div className={s.inputCont}>
-                        <label htmlFor="username">username</label>
-                        <input type="username" />
+                <form className={s.formCont}>
+                    <img className="mb-4" src={logo} alt="" width="72" height="57" />
+                    <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+
+                    <div className="form-floating">
+                    <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
+                    <label htmlFor="floatingInput">Email address</label>
                     </div>
-                    <div className={s.inputCont}>
-                        <label htmlFor="password">password</label>
-                        <input type="password" />
+                    <div className="form-floating">
+                    <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
+                    <label htmlFor="floatingPassword">Password</label>
                     </div>
-                    <button className={s.loginBtn} type='button'>Log In</button>
-                </form>
+
+                    <div className="form-check text-start my-3">
+                    <input className="form-check-input" type="checkbox" value="remember-me" id="flexCheckDefault" />
+                    <label className="form-check-label" htmlFor="flexCheckDefault">
+                        Remember me
+                    </label>
+                    </div>
+                    <button className="btn btn-primary w-100 py-2" type="submit">Sign in</button>
+                    <p className="mt-5 mb-3 text-body-secondary">© 2017–2024</p>
+                </form>   
             </div>
         </>
     )

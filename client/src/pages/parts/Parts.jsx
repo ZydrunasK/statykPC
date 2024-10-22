3d76ccf6eee8170eb36018185ee8828a38119c66
ZydrunasK <kniukstaz@gmail.com>
2024-10-22 16:25:33 +0300

    up

--- client/src/pages/parts/Parts.jsx
+++ client/src/pages/parts/Parts.jsx
@@ -1,6 +1,50 @@
 import s from './parts.module.css';
 
 export function Parts() {
+    const parts = [
+        {
+            id: 1,
+            type: 'mobo',
+            name: 'Gigabyte b550 ds3h',
+            chipset: 'AMD b550',
+            memory: 'DDR4',
+            formFactor: 'micro ATX'
+        },
+        {
+            id: 2,
+            type: 'mobo',
+            name: 'Asus b550M-K',
+            chipset: 'AMD b550',
+            memory: 'DDR4',
+            formFactor: 'micro ATX'
+        },
+        {
+            id: 3,
+            type: 'mobo',
+            name: 'Asrock b550M Steel legend',
+            chipset: 'AMD b550',
+            memory: 'DDR4',
+            formFactor: 'micro ATX'
+        },
+        {
+            id: 4,
+            type: 'mobo',
+            name: 'Asrock b450M-HDV R4.0',
+            chipset: 'AMD b450',
+            memory: 'DDR4',
+            formFactor: 'micro ATX'
+        },
+    ];
+    const partsInTable = parts.map(
+        part => 
+        <tr key={part.id} className={s.tr}>
+            <td className={s.td}>{part.name}</td>
+            <td className={s.td}>{part.chipset}</td>
+            <td className={s.td}>{part.formFactor}</td>
+            <td className={s.td}>{part.memory}</td>
+        </tr>
+        );
+
     return (
         <main className={s.main}>
 
@@ -29,7 +73,16 @@ export function Parts() {
             </div>
 
             <div className={s.partsList}>
-                <h2>parts</h2>
+                <table className={s.partsTable}>
+                    <tr className={s.tr}>
+                        <th className={s.th}>name</th>
+                        <th className={s.th}>chipset</th>
+                        <th className={s.th}>Form Factor</th>
+                        <th className={s.th}>memory</th>
+                    </tr>
+                    {partsInTable}
+                </table>
+
             </div>
         </main>
     );

a55cf7b1b11ea240ae8a00d7b20439fabdc39bcf
ZydrunasK <kniukstaz@gmail.com>
2024-11-15 14:29:50 +0200

    up

--- client/src/components/BuildPartsList.jsx
+++ client/src/components/BuildPartsList.jsx
@@ -10,12 +10,14 @@ export function BuildPartsList() {
         'Vaizdo plokštė',
         'Maitinimo blokas'
     ]
-
-    
+    function handleChoosePart(e) {
+        console.log(e);
+    }
     const html = partTypeList.map((part, id) => <div key={id} className={s.partDiv}>
         <img src="#" alt="partPIC" />
         <p className={s.para}>{part}</p>
-        <p className={s.para}>PART NAME / CHOOSE PART</p>
+        <p className={s.para}>PART NAME</p>
+        <button onClick={handleChoosePart}>CHOOSE PART</button>
     </div>);
  
     return (

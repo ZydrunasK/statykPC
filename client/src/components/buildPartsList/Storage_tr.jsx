b60a9fa84ba3fdfa1ffeed586a816b3362c5bfb5
ZydrunasK <kniukstaz@gmail.com>
2024-12-20 19:16:08 +0200

    up

--- client/src/components/buildPartsList/Storage_tr.jsx
+++ client/src/components/buildPartsList/Storage_tr.jsx
@@ -9,26 +9,27 @@ export function Storage_tr() {
     const navigate = useNavigate();
     const {build} = useContext(BuildContext);
 
-    let list = [<tr className={s.tr} key={354}>
+    
+        let list = <tr className={s.tr} key={354}>
         <td  className={s.td}><img src="#" alt="partPIC" /></td>
         <td  className={s.td}>Kietasis diskas</td>
         <PartNameTd partType={'kietiejiDiskai'} partNum={0} />
         <td  className={s.td}>
             <button onClick={() => navigate(`/dalys/Kietieji-diskai`)}>CHOOSE PART</button>
         </td>
-     </tr>];
+     </tr>;
 
+    if (build.kietiejiDiskai.length !== 0) {
+        list = build.kietiejiDiskai.map((_, i) => <tr className={s.tr} key={i + 354}>
+            <td  className={s.td}><img src="#" alt="partPIC" /></td>
+            <td  className={s.td}>Kietasis diskas</td>
+            <PartNameTd partType={'kietiejiDiskai'} partNum={i} />
+            <td  className={s.td}>
+                <button onClick={() => navigate(`/dalys/Kietieji-diskai`)}>CHOOSE PART</button>
+            </td>
+        </tr>);
+    }
 
-       for (let i = 0; i < build.kietiejiDiskai.length; i++) {
-        list = list.push(<tr className={s.tr} key={i + 354}>
-                    <td  className={s.td}><img src="#" alt="partPIC" /></td>
-                    <td  className={s.td}>Kietasis diskas</td>
-                    <PartNameTd partType={'kietiejiDiskai'} partNum={i + 1} />
-                    <td  className={s.td}>
-                        <button onClick={() => navigate(`/dalys/Kietieji-diskai`)}>CHOOSE PART</button>
-                    </td>
-                 </tr>);  
-       }    
 
     console.log("list length: ", build.kietiejiDiskai.length);
     console.log("list: ", list);
